b616d703a89e3d41212cfe68f2a44c1b68b86e94
xuhuada <459798435@qq.com>
2015-02-12 01:18:28 +0800

    Update formulatab.cpp

--- sources/formulatab.cpp
+++ sources/formulatab.cpp
@@ -14,10 +14,10 @@ Formulatab::~Formulatab() {
 
 int Formulatab::addAtom(_formula* f){
     formula_list.push_back(f);
-    return formula_list.size()-1;
+    return formula_list.size();
 }
 _formula* Formulatab::getAtom(int id){
-    return formula_list[id];
+    return formula_list[id-1];
 }
 
 vector<_formula*> Formulatab::get_formula_list() const {
